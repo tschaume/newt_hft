8454a32183ed7db1a3e12e5e691a64a0cb669a96
Patrick Huck <phuck@lbl.gov>
2014-02-09 22:14:57 -0500

    add config, logging, argparse, xfer_to

---
+++ config.py
@@ -0,0 +1,2 @@
+API = 'https://newt.nersc.gov/newt/'
+ENDPOINT = API + 'file/pdsf/project/projectdirs/star/www/hft'
--- newt_hft.py
+++ newt_hft.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 
-import requests, os
+import requests, os, logging, argparse
+from config import *
 
-# session and newt base url
-s = requests.Session()
-api = 'https://newt.nersc.gov/newt/'
+if __name__ == '__main__':
 
-# authenticate
-username = os.environ['NEWT_USER']
-password = os.environ['NEWT_PWD']
-payload = 'username=' + username + '&password=' + password
-s.post(api + 'auth', data = payload)
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "basepath", help="""
+    base path of output files for respective RunId,
+    is assumed to end in YYYY/DDD/RUN (no extension),
+    {.pdf,.root} will be appended and copied to according location on PDSF,
+    any part of the path before YYYY/DDD/RUN will be ignored
+    """)
+  parser.add_argument("--log", help="show log output", action="store_true")
+  args = parser.parse_args()
+  loglevel = 'DEBUG' if args.log else 'WARNING'
+  logging.basicConfig(
+    format='%(message)s', level=getattr(logging, loglevel)
+  )
 
-# list hft file contents
-endpoint = api + 'file/pdsf/project/projectdirs/star/www/hft'
-r = s.get(endpoint)
-print r.json()
+  s = requests.Session()
+  username = os.environ['NEWT_USER']
+  password = os.environ['NEWT_PWD']
+  payload = 'username=' + username + '&password=' + password
+  r_auth = s.post(API + 'auth', data = payload)
+  logging.debug(r_auth.content)
+  r_list = s.get(ENDPOINT)
+  logging.debug(r_list.content)
+  xfer_to = '/'.join([ENDPOINT] + args.basepath.split('/')[-3:])
+  logging.debug(xfer_to)
+
